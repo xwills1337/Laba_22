d82d8ea6ba27f57d95466f4a9c70243f690e06c3
xwills1337 <misha.sid@rambler.ru>
2023-05-18 00:13:38 +0400

    Добавил функцию сортировки пузырьком

--- Laba_22/Source.cpp
+++ Laba_22/Source.cpp
@@ -3,6 +3,12 @@
 #include <conio.h>
 #include <vector>
 
+struct stats {
+	size_t comparison_count = 0;
+	size_t copy_count = 0;
+};
+
+
 size_t lcg()
 {
 	static size_t x = 0;
@@ -19,8 +25,9 @@ void print(std::vector<int> mas)
 	std::cout << std::endl;
 }
 
-std::vector<int> fill(std::vector<int> mas, int n)
+std::vector<int> random_fill(int n)
 {
+	std::vector<int> mas;
 	for (int i = 0; i < n; i++)
 	{
 		mas.push_back(lcg());
@@ -28,9 +35,40 @@ std::vector<int> fill(std::vector<int> mas, int n)
 	return mas;
 }
 
+stats sort_bubble(std::vector<int>& mas)
+{
+	stats obj;
+	obj.comparison_count = 0;
+	obj.copy_count = 0;
+	for (int i = 0; i < mas.size(); i++)
+	{
+		for (int j = 0; j < mas.size() - 1 - i; j++)
+		{
+			if (mas[j] > mas[j + 1])
+			{
+				int k = mas[j];
+				mas[j] = mas[j + 1];
+				mas[j + 1] = k;
+				obj.copy_count++;
+			}
+			obj.comparison_count++;
+		}
+	}
+	return obj;
+}
+
+void test_sort_bubble(std::vector<int>& mas)
+{
+	stats obj = sort_bubble(mas);
+
+	std::cout << "comparison_count: " << obj.comparison_count << "\ncopy_count: " << obj.copy_count << std::endl;
+}
+
 int main()
 {
 	std::vector<int> mas;
-	mas = fill(mas, 100);
+	mas = random_fill(100);
+	print(mas);
+	test_sort_bubble(mas);
 	print(mas);
 }
